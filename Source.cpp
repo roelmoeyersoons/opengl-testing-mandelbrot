6f802714fbd4c720cc5ebd0c400eea6d81b9a75c
Roel Moeyersoons <Roel.Moeyersoons@net-it.be>
2022-04-06 13:41:59 +0200

    Draw square using EBO, also change primitive

--- Source.cpp
+++ Source.cpp
@@ -93,25 +93,31 @@ int main()
     glDeleteShader(fragmentShader);
 
     float vertices[] = {
-        // first triangle
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f,  0.5f, 0.0f,  // top left 
-        // second triangle
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
     };
 
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO); //also a state object, remembers how we configured the VBO, needs to be done before setting VBO properties
     //when VBO configuration is done, we can unbind it, make other VAO's and configure those. when drawing, select the VAO to use by binding again
+    //also configure EBO!, VAO remembers last bound EBO before unbinding VAO, to check: also remembers bound VBO or just the configuration?
 
     unsigned int VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //GL_DYNAMIC_DRAW, GL_STREAM_DRAW
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -121,16 +127,17 @@ int main()
     //...
 
     glUseProgram(shaderProgram); //state change call ~ from now use this program, doesn't matter how many times called
-    glBindVertexArray(VAO);
-    
 
-    
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 
     while (!glfwWindowShouldClose(window))
     {
+        glBindVertexArray(VAO);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
 

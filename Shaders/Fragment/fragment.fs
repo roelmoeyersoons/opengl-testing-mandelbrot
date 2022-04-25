f8d36da71ea450755f66f7df087d80059f6bbab5
Roel Moeyersoons <Roel.Moeyersoons@net-it.be>
2022-04-25 22:34:05 +0200

    generate mandelbrot

--- Shaders/Fragment/fragment.fs
+++ Shaders/Fragment/fragment.fs
@@ -1,12 +1,50 @@
 #version 330 core
-out vec4 FragColor;  
-in vec3 ourColor;
-in vec2 fTexCoords;
 
-uniform sampler2D texture1;
-uniform sampler2D texture2;
-  
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 fragCoords;
+
+//uniform sampler2D texture1;
+//uniform sampler2D texture2;
+
+const unsigned int ITERATIONS = 100;
+const unsigned int INFINITY = 100000;
+
+const vec3 a = vec3(0.5, 0.5, 0.5);
+const vec3 b = vec3(0.5, 0.5, 0.5);
+const vec3 c = vec3(1.0, 1.0, 0.5);
+const vec3 d = vec3(0.80, 0.90, 0.30);
+
+vec3 palette( in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d )
+{
+    return a + b*cos( 6.28318*(c*t+d) );
+}
+
 void main()
 {
-    FragColor = mix(texture(texture1, fTexCoords), texture(texture2, fTexCoords), 0.3);
+
+    float real = fragCoords.x * 1.3f - 0.5f;
+    float imag = fragCoords.y * 1.3f;
+    float accReal = 0.0f;
+    float accImag = 0.0f;
+    float vecSize = 0.0f;
+
+    int i = 0;
+    while(i < ITERATIONS && vecSize < INFINITY){
+        float previousAccReal = accReal;
+        accReal = ((previousAccReal * previousAccReal) - (accImag * accImag)) + real;
+        accImag = 2*(previousAccReal * accImag) + imag;
+
+        vecSize = accReal * accReal - accImag * accImag;
+        i++;
+    }
+
+    float normalized = float(i)/ITERATIONS;
+    vec3 color = palette(normalized, a, b, c, d);
+    //vec3 color = vec3(normalized, 0.0f, 0.0f);
+
+    FragColor = vec4(color, 1.0f);
+    //FragColor = mix(texture(texture1, fTexCoords), texture(texture2, fTexCoords), 0.3);
 }
+
--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,7 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <Windows.h>
 
 #include "Include/ShaderLoader.h"
 #include "Include/Camera.h"
@@ -21,8 +22,8 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 1200;
+const unsigned int SCR_HEIGHT = 900;
 
 // camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
@@ -62,7 +63,7 @@ int main()
     glfwSetScrollCallback(window, scroll_callback);
 
     // tell GLFW to capture our mouse
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // glad: load all OpenGL function pointers
     // ---------------------------------------
@@ -86,27 +87,17 @@ int main()
     //    -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left 
     //};
 
-    
-    float x = -sqrtf(2.0/ 9);
-
     float vertices[] = {
-        0.0f, 1.0f, 0.0f,  1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-        0.0f, -0.33f, sqrtf(8.0/9),   1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-        sqrtf(2.0 / 3), -0.33f, -sqrtf(2.0/9),   0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-        -sqrtf(2.0 / 3), -0.33f, -sqrtf(2.0/9),   0.0f, 1.0f,  0.0f, 0.0f, 0.0f,
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        1.0f, 1.0f, 0.0f,
+        -1.0f, 1.0f, 0.0f,
     };
 
     unsigned int indices[] = {
-        0,1,2,
-        0,1,3,
-        0,2,3,
-        1,2,3,
+        0, 1, 2, // first triangle
+        0, 3, 2  // second triangle
     };
-
-    //unsigned int indices[] = {
-    //    0, 1, 3, // first triangle
-    //    1, 2, 3  // second triangle
-    //};
 
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -122,102 +113,21 @@ int main()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // texture coord attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+    //glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    //glEnableVertexAttribArray(1);
 
-    // color attribute
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(5 * sizeof(float)));
-    glEnableVertexAttribArray(2);
+    //// color attribute
+    //glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(5 * sizeof(float)));
+    //glEnableVertexAttribArray(2);
 
 
-    // load and create a texture 
-    // -------------------------
-    unsigned int texture1, texture2;
-    // texture 1
-    // ---------
-    glGenTextures(1, &texture1);
-    glBindTexture(GL_TEXTURE_2D, texture1);
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load image, create texture and generate mipmaps
-    int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char* data = stbi_load("Resources/textures/container.jpg", &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
-    // texture 2
-    // ---------
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load image, create texture and generate mipmaps
-    data = stbi_load("Resources/textures/awesomeface.png", &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
 
     // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
     // -------------------------------------------------------------------------------------------
     ourShader.use();
-    ourShader.setInt("texture1", 0);
-    ourShader.setInt("texture2", 1);
-
-
-    glm::vec3 positions[] = {
-        glm::vec3(0.0f,  0.0f,  -0.0f),
-        glm::vec3(2.0f,  5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3(2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f,  3.0f, -7.5f),
-        glm::vec3(1.3f, -2.0f, -2.5f),
-        glm::vec3(1.5f,  2.0f, -2.5f),
-        glm::vec3(1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
-    };
-
-    srand(time(NULL));
-
-    int rotationInversed[] = {
-        rand() % 3 - 1,
-        rand() % 3 - 1,
-        rand() % 3 - 1,
-        rand() % 3 - 1,
-        rand() % 3 - 1,
-        rand() % 3 - 1,
-        rand() % 3 - 1,
-        rand() % 3 - 1,
-        rand() % 3 - 1,
-        rand() % 3 - 1,
-    };
 
     glEnable(GL_DEPTH_TEST);
     // render loop
@@ -227,6 +137,8 @@ int main()
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
+
+        SleepEx(100, false);
 
 
         // input
@@ -240,49 +152,10 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // bind textures on corresponding texture units
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
-
-        // create transformations
-       //glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-       //transform = glm::translate(transform, glm::vec3(0.0f, 0.0f, 0.3f));
-       //transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 0.0f));
-
-
-        glm::mat4 view = camera.GetViewMatrix();
-
-        glm::mat4 proj = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-
-        // get matrix's uniform location and set matrix
-
-        unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-
-        unsigned int projLoc = glGetUniformLocation(ourShader.ID, "projection");
-        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
 
         glBindVertexArray(VAO);
-        for (int i = 0; i < 10; i++) {
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, positions[i]);
-            model = glm::rotate(model, (float)glfwGetTime()* rotationInversed[i], glm::vec3(0.0f, 1.0f, 0.0f));
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-            unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
-            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-            // render container
-            glBindVertexArray(VAO);
-            glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
-        }
-
-        
-
-        
-
-        
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
